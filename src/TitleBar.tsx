c41d7214aaa6e64554d8e7263eb8300f8a26d5d4
István Antal <istvan.m.antal@gmail.com>
2016-04-27 12:21:22 +0100

    Menu item toggle

--- src/TitleBar.tsx
+++ src/TitleBar.tsx
@@ -1,15 +1,37 @@
 import React = require('react');
 
-export class TitleBar extends React.Component<{}, {}> {
+interface ComponentState {
+    activeItem: string
+}
+
+export class TitleBar extends React.Component<{}, ComponentState> {
+    private items = [
+        { name: 'select', icon: 'mouse' },
+        { name: 'rect', icon: 'cloud' }
+    ];
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            activeItem: 'select'
+        };
+    }
     render() {
+        const items = this.items.map((item, index) => {
+            const className = "icon icon-" + item.icon;
+            let buttonClasses = "btn btn-default";
+            if (item.name === this.state.activeItem) {
+                buttonClasses += " active";
+            }
+            return <button key={index} onClick={ () => { this.setState({  activeItem: item.name }) } } className={buttonClasses}>
+                <span className={className}></span>
+            </button>
+        });
         return (
             <header className="toolbar toolbar-header">
                 <h1 className="title">Drawing Board</h1>
                 <div className="toolbar-actions">
                     <div className="btn-group">
-                        <button className="btn btn-default active">
-                            <span className="icon icon-cloud"></span>
-                        </button>
+                        {items}
                     </div>
                 </div>
             </header>
